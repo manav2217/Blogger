0d6d6a4bd9287f19ff416d09033ca069934121fb
manav2217 <manavprajapati2217@gmail.com>
2021-08-17 15:58:18 +0530

    new

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ck+qr4q*_+=&qk_kp7=w3!tx6d8itjub4hq2ujr++mfbujbx#_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['blogger-india.herokuapp.com' , '127.0.0.1']
 
