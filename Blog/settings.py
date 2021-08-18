0315c1c39a29c98953aabbfbaca5332f7fcd9425
manav2217 <manavprajapati2217@gmail.com>
2021-08-18 15:10:46 +0530

    new

--- Blog/settings.py
+++ Blog/settings.py
@@ -92,11 +92,11 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-      'default': {
-          'ENGINE': 'djongo',
-          'NAME': 'blogdatabase',
-      }
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
